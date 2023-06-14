c1a6ea018ba417b89137dd5f5d22a68c6f39b3bd
Wahyu Kristiawan <whykris97@gmail.com>
2023-06-14 19:26:26 +0700

    add condition

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -15,7 +15,7 @@ class NewsController extends Controller
 
     public function index()
     {
-        $news = NewsArticle::select(['id', 'title', 'slug', 'image', 'created_at'])->get();
+        $news = NewsArticle::select(['id', 'title', 'slug', 'image', 'created_at'])->where('is_publish', 1)->get();
         foreach ($news as $key => $value) {
             $news[$key]->views = 1;
             $news[$key]->image = asset("storage/" . $value->image);
