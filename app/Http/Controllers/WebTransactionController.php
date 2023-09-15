c5846796cf9c9e64ecbf94d5d93bfd2987dae436
primusnathan11 <nathan.primus77@gmail.com>
2023-09-15 14:41:39 +0700

    fix transaction bingkai karya

--- app/Http/Controllers/WebTransactionController.php
+++ app/Http/Controllers/WebTransactionController.php
@@ -40,7 +40,7 @@ class WebTransactionController extends Controller
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = true;
+        \Midtrans\Config::$isProduction = false;
         // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
