068e67c061e715476d25aa7f276daa7e39d723ee
devisap <ilham.sagitaputra@gmail.com>
2023-09-18 13:10:39 +0700

    fixing isproduction midtrans

--- app/Http/Controllers/WebTransactionController.php
+++ app/Http/Controllers/WebTransactionController.php
@@ -8,10 +8,10 @@ use App\Models\WebTransaction;
 use App\Models\EmailUser;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Mail;
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+// use Illuminate\Support\Facades\Mail;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+// use PHPMailer\PHPMailer\Exception;
 
 
 class WebTransactionController extends Controller
@@ -40,7 +40,7 @@ class WebTransactionController extends Controller
         // Set your Merchant Server Key
         \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
         // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
+        \Midtrans\Config::$isProduction = true;
         // Set sanitization on (default)
         \Midtrans\Config::$isSanitized = true;
         // Set 3DS transaction for credit card to true
@@ -58,32 +58,32 @@ class WebTransactionController extends Controller
         );
 
         $snapToken = \Midtrans\Snap::getSnapToken($params);
-        $mail = new PHPMailer(true);
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'write email here';                     //SMTP username
-            $mail->Password   = 'write password here';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        // $mail = new PHPMailer(true);
+        // try {
+        //     //Server settings
+        //     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        //     $mail->isSMTP();                                            //Send using SMTP
+        //     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        //     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        //     $mail->Username   = 'write email here';                     //SMTP username
+        //     $mail->Password   = 'write password here';                               //SMTP password
+        //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        //     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            //Recipients
-            $mail->setFrom('write email here', 'BumiBaik');
-            $mail->addAddress($request->email);
+        //     //Recipients
+        //     $mail->setFrom('write email here', 'BumiBaik');
+        //     $mail->addAddress($request->email);
 
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Donasi anda telah berhasil masuk';
-            $mail->Body    = 'Terima kasih donasi anda telah berhasil terkumpulkan. Anda akan mendapatkan email dari detail kegiatan ini.';
+        //     //Content
+        //     $mail->isHTML(true);                                  //Set email format to HTML
+        //     $mail->Subject = 'Donasi anda telah berhasil masuk';
+        //     $mail->Body    = 'Terima kasih donasi anda telah berhasil terkumpulkan. Anda akan mendapatkan email dari detail kegiatan ini.';
 
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
+        //     $mail->send();
+        //     echo 'Message has been sent';
+        // } catch (Exception $e) {
+        //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        // }
         return view('landing.donate.checkout',compact('snapToken', 'transaction'));
     }
 
